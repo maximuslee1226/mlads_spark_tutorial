17b383e707ab1cb07ae55d2bbafe8d8d134f5832
Brandon <maximuslee@sbcglobal.net>
2017-08-21 15:20:51 -0700

    Update verification.sh

--- verification.sh
+++ verification.sh
@@ -2,11 +2,11 @@ mkdir /tmp/verif
 whoami > /tmp/verif/whoami
 pwd > /tmp/verif/pwd
 
-cd /home/diispark
+cd /home/remoteuser
 
 if [[ -f ClusterVerification.R ]]; then sudo rm -Rf ClusterVerification.R; fi;
 
 # UPDATE THE URL FOR MLADS
-wget https://raw.githubusercontent.com/akzaidi/mlads_spark_tutorial/master/ClusterVerification.R
+wget https://raw.githubusercontent.com/maximuslee1226/mlads_spark_tutorial/master/ClusterVerification.R
 
-Rscript --default-packages= /home/diispark/ClusterVerification.R > /tmp/verif/rout.log
+Rscript --default-packages= /home/remoteuser/ClusterVerification.R > /tmp/verif/rout.log
