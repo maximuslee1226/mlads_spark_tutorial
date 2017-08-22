3e8c2ea853e36491fe6a0bbc8b7d32eb504ffc62
Brandon <maximuslee@sbcglobal.net>
2017-08-21 17:22:22 -0700

    Update ClusterVerification.R

--- ClusterVerification.R
+++ ClusterVerification.R
@@ -27,7 +27,7 @@ rxOptions(fileSystem = RxHdfsFileSystem())
 
 # Specify hdfsShareDir and shareDir so that
 # RxSpark will work even if we run as root
-computeContext <- RxSpark(consoleOutput=TRUE,
+computeContext <- RxSparkConnect(consoleOutput=TRUE,
 	hdfsShareDir = "/user/RevoShare/remoteuser",
 	shareDir = "/var/RevoShare/remoteuser")
 rxSetComputeContext(computeContext)
